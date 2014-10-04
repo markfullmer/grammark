c8dc1adb1d73335269c43545f9de73804911781c
markfullmer <mfullmer@gmail.com>
2014-10-04 16:17:03 -0500

    adding templates & refactoring Google Analytics include

--- example.settings.php
+++ example.settings.php
@@ -4,3 +4,4 @@ define('USERNAME','');
 define('PASSWORD','');
 define('DB_NAME','');
 define('EMAIL',''); // for the contact and database suggestion forms
+define('ANALYTICS',''); // place Google Analytics tracking code here
--- index.php
+++ index.php
@@ -1,62 +1,49 @@
 <?php
 session_start();
 require_once 'vendor/autoload.php';
-
+require_once 'settings.php';
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
 
 ));
 //  add to above for caching: 'cache' => 'cache/compilation_cache',
-echo $twig->render('index.twig', array('name' => 'Fabien'));
-include('microtimestart.php');
-include('settings.php');
-include('includes/head.tpl.php');
-?>
-<?php echo '<body id="tab'. $tab .'">';
-//include_once('js/analyticstracking.php');
-include('includes/nav.tpl.php');
-?>
-<div id='wrap'>
 
-<?php
 $pages = array(
-	'about',
-	'source',
-	'contact',
-	'wordiness-list',
-	'grammar-error-list',
-	'transitions-list',
-	'database-add'
+	'about', 'source', 'contact', 'wordiness-list', 'grammar-error-list',
+	'transitions-list', 'database-add'
 );
-if (isset($_GET['id']) && $_GET['id'] == 'start' ) { $_SESSION['text'] = '0'; }
-if (isset($_POST['text'])) { $_SESSION['text'] = $_POST['text'];}
-if (isset($_GET['url']) && in_array($_GET['url'],$pages)) {
-	include("includes/" . $_GET['url'] . ".php");
+$page = 'default';
+$data = array();
+if (ANALYTICS != '') {
+	$data['analytics'] = ANALYTICS;
 }
-elseif (isset($_SESSION['text']) && $_SESSION['text'] != '0') {
-	include('mysql.php');
-	include ('includes/menu.php');
-	include('variables.php');
+if (isset($_GET['id']) && $_GET['id'] == 'start' ) {
+  // A "start" callback wipes the session
+	unset($_SESSION['text']);
+}
+
+// If text has just been submitted, place it in the session object
+if (isset($_POST['text'])) { $_SESSION['text'] = $_POST['text'];}
+
+if (isset($_GET['url']) && in_array($_GET['url'],$pages)) {
+	// If the callback is for a page defined in the system
+	$page = 'page';
+	$data = array('content' => 'hello','sidebar' => 'mike');
+	//$data = getContent('page',$_GET['url']);
+}
+elseif (isset($_SESSION['text'])) {
+	// Some text has been submitted
 	if (empty($_GET['id'])) {
-	  include('includes/default.php');
+		// No specific fix tab is selected
+	  $page = 'results';
+	  // $data = getContent('results',0);
 	}
 	else {
-		$page = $_GET['id'];
-		include('includes/' . $page . '.php');
-		include('includes/output.php');
+		// A specific fix tab is selected
+		$page = 'fix';
+		// $data = getContent('fix',$_GET['id'])
 	}
 }
-else {
-	include ('includes/form.php');
-	include ('includes/sidebar.tpl.php');
-}
+echo $twig->render($page . '.twig', $data);
 ?>
-</div>
-<script src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.5/js/vendor/jquery.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.4.5/js/foundation.min.js"></script>
-<script>
-   $(document).foundation();
-</script>
-</body>
-</html>
 
