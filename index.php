90c16197dc281516b15c981fb1c1e9e1f31e151c
markfullmer <mfullmer@gmail.com>
2014-10-26 08:50:28 -0500

    setting twig caching on by default

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'vendor/autoload.php';
 require_once 'settings.php';
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
+'cache' => 'cache/compilation_cache',
 ));
 //  add to above for caching: 'cache' => 'cache/compilation_cache',
 
