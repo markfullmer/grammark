ff4972c2421d9a87e4d5562cf02d6396c975ac20
markfullmer <mfullmer@gmail.com>
2015-05-12 13:11:24 -0700

    filling out resources pages

--- controllers/resource.js
+++ controllers/resource.js
@@ -30,5 +30,19 @@ function resourceCtrl ($scope, $routeParams, type) {
             }
             $scope.data = table;
             break;
+        case 'grammar':
+            $scope.title = "Grammar";
+            type.get('grammar');
+            table = [];
+            count = 0;
+            values = type.data.corrections;
+            for (var i in values) { 
+                table.push({
+                    find: i,
+                    suggestion: values[i],
+                });
+            }
+            $scope.data = table;
+            break;
     }
 }
