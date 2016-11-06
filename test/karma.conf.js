e3dd40d6a977277eacc6ac890872b6a298bce362
markfullmer <mfullmer@gmail.com>
2016-11-06 09:04:00 -0600

    Commit #9 to dist build

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,8 +25,8 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-underscore-module/angular-underscore-module.js',
       'bower_components/underscore/underscore.js',
+      'bower_components/angular-underscore-module/angular-underscore-module.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
